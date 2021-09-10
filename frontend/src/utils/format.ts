f183997f729e24f0205d2a25c5fa41a06610bd5f
David Amorim <davidfilipeamorim@gmail.com>
2021-09-10 17:43:40 +0100

    DataTable integration

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,5 +1,13 @@
+import { format } from 'date-fns';
+
 export const round = (value: number, precision = 0): number => {
   const multiplier = Math.pow(10, precision);
 
   return Math.round(value * multiplier) / multiplier;
+}
+
+export const formatLocalDate = (date: string, pattern: string): string => {
+  const dt = new Date(date);
+  const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+  return format(dtDateOnly, pattern);
 }
